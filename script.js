bd280014f226b35f98f0cc92acc89992eaae99b2
Veniamin <bm0651@yandex.ru>
2023-12-02 17:58:46 +0300

    lesson 4

---
+++ script.js
@@ -0,0 +1,38 @@
+const btnLeft = document.getElementById("left");
+const btnUp = document.getElementById("up");
+const btnDown = document.getElementById("down");
+const btnRight = document.getElementById("right");
+let monitor = document.getElementById('mon');
+const folder = "img/tanks/";
+const arrTrans = ["logan", "samosval", "t34", "t90", "uaz"];
+let curTrans = 0;
+
+function runLeft(){
+    console.log("Поехали влево");
+    let path = `${folder}${arrTrans[curTrans]}/left.jpg`
+    monitor.setAttribute('src', path);
+}
+function runUp(){
+    console.log("Поехали вперёд");
+    let path = `${folder}${arrTrans[curTrans]}/up.jpg`
+    monitor.setAttribute('src', path);
+}
+
+function runDown(){
+    console.log("Поехали назад");
+    let path = `${folder}${arrTrans[curTrans]}/down.jpg`
+    monitor.setAttribute('src', path);
+}
+function runRight(){
+    console.log("Поехали вправо");
+    let path = `${folder}${arrTrans[curTrans]}/right.jpg`
+    monitor.setAttribute('src', path);
+}
+
+btnLeft.addEventListener('click', runLeft);
+btnUp.addEventListener('click', runUp);
+btnDown.addEventListener('click', runDown);
+btnRight.addEventListener('click', runRight);
+
+
+
